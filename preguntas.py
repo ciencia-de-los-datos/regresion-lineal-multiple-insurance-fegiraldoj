f1bc9bb576321fbffd6b48fceda775e38439059d
fegiraldoj <101747510+fegiraldoj@users.noreply.github.com>
2022-05-13 19:45:25 -0500

    Response 03

--- preguntas.py
+++ preguntas.py
@@ -101,11 +101,12 @@ def pregunta_03():
             # Paso 2: Construya un selector de características que seleccione las K
             # características más importantes. Utilice la función f_regression.
             (
-                "selectKBest",SelectKBest(score_func=f_regression),
+                "selectKBest",
+                 SelectKBest(score_func=f_regression),
             ),
             # Paso 3: Construya un modelo de regresión lineal.
             (
-                "LinearRegression",
+                "LR",
                 LinearRegression(),
             ),
         ],
@@ -116,7 +117,9 @@ def pregunta_03():
 
     # Defina un diccionario de parámetros para el GridSearchCV. Se deben
     # considerar valores desde 1 hasta 11 regresores para el modelo
-    param_grid = { "selectKBest__K": (1, 11), }
+    param_grid = {
+        "selectKBest__K":(1, 11), 
+    }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
     # parámetros. Use cv = 5, y como métrica de evaluación el valor negativo del
